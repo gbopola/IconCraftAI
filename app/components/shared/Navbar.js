69ba7d46ab6d41a4535e3b4a7595f8237336f7f1
Runanka <runanka@outlook.com>
2024-02-05 17:04:12 +0530

    Fix initial page load issue with Sign-In button

--- app/components/shared/Navbar.js
+++ app/components/shared/Navbar.js
@@ -69,13 +69,19 @@ const Navbar = () => {
               </div>
             ) : (
               <div>
-                <button
-                  type="button"
-                  onClick={() => signIn("google")}
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                <form
+                  action={async () => {
+                    await signIn("google");
+                  }}
                 >
-                  Sign in
-                </button>
+                  <button
+                    //type="button"
+                    //onClick={() => signIn("google")}
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign in
+                  </button>
+                </form>
               </div>
             )}
           </div>
