51951f3d35273c54f3960acec75e8706fb42fcb9
gbopola <opeafolabi5@gmail.com>
2024-06-18 13:40:20 +0100

    remove react hook form

--- components/generate/GenerateForm.js
+++ components/generate/GenerateForm.js
@@ -1,146 +1,236 @@
 "use client";
-import React, { useContext, useState, useEffect, use } from "react";
-import { useForm } from "react-hook-form";
-import Image from "next/image";
-import { CheckIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
-import { GenerateIconContext } from "../../context/GenerateIconContext";
+import React, { useContext, useEffect, useState } from "react";
+import Tabs from "./Tabs";
 import PromptInfo from "./PromptInfo";
+import { GenerateIconContext } from "../../context/GenerateIconContext";
+import { CheckIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { iconStyles } from "../../constants/main";
+import gradient from "../../../public/assets/images/gradient.png";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
 import ErrorAlert from "./ErrorAlert";
-import { classes, iconStyles } from "../../constants/main";
-import { LoadingSpinner } from "./LoadingSpinner";
-import { Slide, toast } from "react-toastify";
 
 const GenerateForm = () => {
-  const { generateIcon, setGenerateIcon } = useContext(GenerateIconContext);
+  const { generateIcon, setGenerateIcon, setGeneratedIcon, setIsGenerated } =
+    useContext(GenerateIconContext);
   const [loading, setLoading] = useState(false);
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    watch,
-    formState: { errors },
-  } = useForm({
-    defaultValues: generateIcon,
+  const [errors, setErrors] = useState({
+    prompt: false,
+    color: false,
+    style: false,
   });
 
-  const selectedColor = watch("color");
-  const selectedStyle = watch("style");
-  const numIcons = watch("numIcons");
+  const { status, data: session } = useSession();
 
-  useEffect(() => {
-    register("color", { required: true });
-    register("style", { required: true });
-
+  // change values in state
+  const handleStateChange = (event) => {
     setGenerateIcon({
       ...generateIcon,
-      color: selectedColor,
-      style: selectedStyle,
-      numIcons,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // change current style
+  const changeCurrentStyle = (event) => {
+    setGenerateIcon({
+      ...generateIcon,
+      style: event.target.id,
+    });
+  };
+
+  // handle select color
+  const handleSelectColor = (event, classType) => {
+    generateIcon.color !== classType.color &&
+      setGenerateIcon({
+        ...generateIcon,
+        color: event.target.id,
+      });
+  };
+
+  // add number of icons
+  const addNumIcons = () => {
+    setGenerateIcon((prevGenerateIcon) => {
+      return {
+        ...prevGenerateIcon,
+        numIcons: prevGenerateIcon.numIcons + 1,
+      };
+    });
+  };
+
+  // subtract number of icons
+  const subtractNumIcons = () => {
+    setGenerateIcon((prevGenerateIcon) => {
+      return {
+        ...prevGenerateIcon,
+        numIcons:
+          prevGenerateIcon.numIcons - 1 < 1 ? 1 : prevGenerateIcon.numIcons - 1,
+      };
+    });
+  };
+
+  // handle generate icon
+  const handleGenerateIcon = async () => {
+    // Reset errors
+    setErrors({
+      prompt: false,
+      color: false,
+      style: false,
     });
 
-    // Check if there are any errors
-    if (Object.keys(errors).length > 0) {
-      toast.error("Some fields are missing", {
-        position: "top-center",
-        autoClose: false,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        transition: Slide,
-      });
+    // Collect error conditions
+    const validationErrors = [];
+
+    // Check for empty fields
+    if (!generateIcon.prompt.trim()) {
+      validationErrors.push("prompt");
     }
-  }, [
-    selectedColor,
-    selectedStyle,
-    numIcons,
-    errors.prompt,
-    errors.color,
-    errors.style,
-  ]);
 
-  const increaseNumIcons = () => {
-    setValue("numIcons", numIcons + 1);
+    if (!generateIcon.color) {
+      validationErrors.push("color");
+    }
+
+    if (!generateIcon.style) {
+      validationErrors.push("style");
+    }
+
+    // Display all collected errors
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      prompt: validationErrors.includes("prompt"),
+      color: validationErrors.includes("color"),
+      style: validationErrors.includes("style"),
+    }));
+
+    // Check if there are any errors before proceeding
+    if (validationErrors.length > 0) {
+      return;
+    }
+
+    // show loading spinner
+    setLoading(true);
+
+    const response = await fetch(`/api/generate/${session?.user.id}`, {
+      method: "POST",
+      body: JSON.stringify(generateIcon),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    //  add generated icon to state
+    setGeneratedIcon(data);
+    setIsGenerated(true);
+    setLoading(false);
   };
 
-  const decreaseNumIcons = () => {
-    setValue("numIcons", Math.max(1, numIcons - 1));
-  };
-
-  const onSubmit = async (data) => {
-    console.log(data);
-    // setLoading(true);
-    // const response = await fetch(`/api/generate/${session?.user.id}`, {
-    //   method: "POST",
-    //   body: JSON.stringify(data),
-    // });
-    // if (!response.ok) {
-    //   throw new Error(`HTTP error! Status: ${response.status}`);
-    // }
-    // const responseData = await response.json();
-    // setGeneratedIcon(responseData);
-    // setIsGenerated(true);
-    // setLoading(false);
-  };
+  // colors
+  const classes = [
+    {
+      id: 1,
+      color: "black",
+      style: "rounded-full bg-black p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 2,
+      color: "red",
+      style: "rounded-full bg-red-500 p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 3,
+      color: "orange",
+      style: "rounded-full bg-orange-500 p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 4,
+      color: "yellow",
+      style: "rounded-full bg-yellow-500 p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 5,
+      color: "green",
+      style: "rounded-full bg-green-500 p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 6,
+      color: "blue",
+      style: "rounded-full bg-blue-500 p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 7,
+      color: "indigo",
+      style: "rounded-full bg-indigo-500 p-5 mr-2 cursor-pointer relative",
+    },
+    {
+      id: 8,
+      color: "violet",
+      style: "rounded-full bg-violet-500 p-5 mr-2 cursor-pointer relative",
+    },
+  ];
 
   return (
     <div className="mt-32 px-20 mx-auto md:w-[630px] w-full">
-      {/* {(errors.prompt || errors.color || errors.style) && (
+      {errors.prompt || errors.color || errors.style ? (
         <ErrorAlert
-          promptError={!!errors.prompt}
-          colorError={!!errors.color}
-          styleError={!!errors.style}
+          promptError={errors.prompt}
+          colorError={errors.color}
+          styleError={errors.style}
         />
-      )} */}
+      ) : null}
       <span className="inline-flex items-center rounded-full bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10 mb-6">
         Generate Icon
       </span>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <h3 className="text-base font-semibold leading-7 text-gray-900">
-            Which elements would you want featured in your icons?
-          </h3>
-          <p className="text-sm leading-7 text-gray-600">
-            Make it short and sweet if you can.
-          </p>
-          <div className="flex items-center">
-            <input
-              type="text"
-              name="prompt"
-              id="prompt"
-              className="inline-block w-full mt-2 rounded-md border-0 py-2 px-4 mr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-0 sm:text-sm sm:leading-6"
-              placeholder="e.g. an angry bear"
-              {...register("prompt", { required: true })}
-            />
-            <div className="relative">
-              <PromptInfo />
-            </div>
+      <div>
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Which elements would you want featured in your icons?
+        </h3>
+        <p className="text-sm leading-7 text-gray-600">
+          Keep it concise and straightforward.
+        </p>
+        <label htmlFor="email" className="sr-only">
+          Email
+        </label>
+        <div className="flex items-center">
+          <input
+            type="email"
+            name="prompt"
+            id="email"
+            className="inline-block  w-full mt-2 rounded-md border-0 py-2 px-4 mr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  outline-0 sm:text-sm sm:leading-6"
+            placeholder="e.g. an angry bear"
+            value={generateIcon.prompt}
+            onChange={handleStateChange}
+          />
+          <div className="relative">
+            <PromptInfo />
           </div>
         </div>
-        <div className="mt-10">
-          <h3 className="text-base font-semibold leading-7 text-gray-900">
-            Which color suits your app the most?
-          </h3>
-          <div className="flex flex-wrap mt-2 items-center">
-            {classes.map((classType) => (
+      </div>
+      <div className="mt-10">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Which color suits your app the most?
+        </h3>
+        {/* <Tabs /> */}
+        <div className="flex flex-wrap mt-2 gap-2 items-center">
+          {classes.map((classType) => {
+            return (
               <div
                 key={classType.id}
                 id={classType.color}
-                className={`rounded-full ${classType.style} p-5 mr-2 cursor-pointer relative`}
-                onClick={() => setValue("color", classType.color)}
+                className={`${classType.style}`}
+                onClick={() => handleSelectColor(event, classType)}
               >
-                {selectedColor === classType.color && (
+                {generateIcon.color === classType.color && (
                   <CheckIcon
                     className="h-4 w-4 text-white absolute top-[11px] right-[11px]"
                     aria-hidden="true"
                   />
                 )}
               </div>
-            ))}
-          </div>
+            );
+          })}
+        </div>
+        <div>
           <h3 className="text-base font-semibold leading-7 text-gray-900 mt-10">
             Which style would you like?
           </h3>
@@ -152,10 +242,10 @@ const GenerateForm = () => {
                   id={style.name}
                   width={100}
                   height={100}
-                  onClick={() => setValue("style", style.name)}
+                  onClick={changeCurrentStyle}
                   alt={style.name}
                   className={`rounded-xl cursor-pointer ${
-                    selectedStyle === style.name &&
+                    generateIcon.style === style.name &&
                     "border border-4 border-purple-600"
                   }`}
                 />
@@ -165,6 +255,7 @@ const GenerateForm = () => {
               </div>
             ))}
           </div>
+
           <h3 className="text-base font-semibold leading-7 text-gray-900 mt-10 mb-3">
             How many icons do you need?
           </h3>
@@ -172,33 +263,35 @@ const GenerateForm = () => {
             <button
               type="button"
               id="minus"
-              onClick={decreaseNumIcons}
-              className="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={subtractNumIcons}
+              className="rounded-full   bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               <MinusIcon id="minus" className="h-5 w-5" aria-hidden="true" />
             </button>
-            <div className="rounded-md border-0 py-2 px-4 mx-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-0 sm:text-sm sm:leading-6">
-              {numIcons}
+            <div className="rounded-md border-0 py-2 px-4 mx-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  outline-0 sm:text-sm sm:leading-6">
+              {generateIcon.numIcons}
             </div>
+
             <button
               type="button"
               id="plus"
-              onClick={increaseNumIcons}
+              onClick={addNumIcons}
               className="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               <PlusIcon id="plus" className="h-5 w-5" aria-hidden="true" />
             </button>
           </div>
           <button
-            type="submit"
             disabled={loading}
-            className="rounded-md flex items-center mt-4 bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={handleGenerateIcon}
+            className={`rounded-md mt-4 bg-indigo-${
+              loading ? "400" : "600"
+            } px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
           >
-            {loading && <LoadingSpinner />}
             {loading ? "Loading..." : "Generate"}
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
